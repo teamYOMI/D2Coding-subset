737719d9f4e8bb1a62af968a7356a93649acace5
TetraTheta <tetratheta@gmail.com>
2021-01-16 14:14:50 +0900

    변환 스크립트 오류 사항 수정 및 반영

--- script/convert.sh
+++ script/convert.sh
@@ -2,14 +2,14 @@
 
 mkdir output
 echo Converting D2Coding font to WOFF...
-pyftsubset "D2Coding.ttf" --flavor="woff" --with-zopfli --output-file="output/D2Coding-subset.woff" --text-file="glyphs.txt" --drop-tables=FFTM --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap --notdef-glyph --notdef-outline --recommended-glyphs --name-legacy --name-IDs='*' --name-languages='*'
+pyftsubset "D2CODING-VER1.3.2-20180524-ALL.TTC" --font-number=0 --flavor="woff" --with-zopfli --output-file="output/D2Coding-subset.woff" --text-file="glyphs.txt" --drop-tables+=FFTM --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap --notdef-glyph --notdef-outline --recommended-glyphs --name-legacy --name-IDs='*' --name-languages='*'
 echo Done.
 echo Converting D2Coding Bold font to WOFF...
-pyftsubset "D2Coding-Bold.ttf" --flavor="woff" --with-zopfli --output-file="output/D2Coding-subset-Bold.woff" --text-file="glyphs.txt" --drop-tables=FFTM --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap --notdef-glyph --notdef-outline --recommended-glyphs --name-legacy --name-IDs='*' --name-languages='*'
+pyftsubset "D2CODING-VER1.3.2-20180524-ALL.TTC" --font-number=1 --flavor="woff" --with-zopfli --output-file="output/D2Coding-subset-Bold.woff" --text-file="glyphs.txt" --drop-tables+=FFTM --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap --notdef-glyph --notdef-outline --recommended-glyphs --name-legacy --name-IDs='*' --name-languages='*'
 echo Done.
 echo Converting D2Coding font to WOFF2...
-pyftsubset "D2Coding.ttf" --flavor="woff2" --output-file="output/D2Coding-subset.woff2" --text-file="glyphs.txt" --drop-tables=FFTM --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap --notdef-glyph --notdef-outline --recommended-glyphs --name-legacy --name-IDs='*' --name-languages='*'
+pyftsubset "D2CODING-VER1.3.2-20180524-ALL.TTC" --font-number=0 --flavor="woff2" --output-file="output/D2Coding-subset.woff2" --text-file="glyphs.txt" --drop-tables+=FFTM --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap --notdef-glyph --notdef-outline --recommended-glyphs --name-legacy --name-IDs='*' --name-languages='*'
 echo Done.
 echo Converting D2Coding Bold font to WOFF2...
-pyftsubset "D2Coding-Bold.ttf" --flavor="woff2" --output-file="output/D2Coding-subset-Bold.woff2" --text-file="glyphs.txt" --drop-tables=FFTM --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap --notdef-glyph --notdef-outline --recommended-glyphs --name-legacy --name-IDs='*' --name-languages='*'
+pyftsubset "D2CODING-VER1.3.2-20180524-ALL.TTC" --font-number=1 --flavor="woff2" --output-file="output/D2Coding-subset-Bold.woff2" --text-file="glyphs.txt" --drop-tables+=FFTM --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap --notdef-glyph --notdef-outline --recommended-glyphs --name-legacy --name-IDs='*' --name-languages='*'
 echo Done.
